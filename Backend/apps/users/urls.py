e9fdf9dd1c79335feffbf59f5ea378c68ad74b2d
nikhilmali1225 <Nikhil.mali@aniruddhagps.com>
2026-02-23 13:22:14 +0530

    changes

--- Backend/apps/users/urls.py
+++ Backend/apps/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import *
-
+# 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="register"),
     path("me/", MeAPIView.as_view(), name="me"),
